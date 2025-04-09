18b355790e9aa7836c4a0e8c4abac53c95345962
₱ØⱠɎ₥ØⱤ₱Ⱨł₵ <54730301+lilmond@users.noreply.github.com>
2025-04-09 08:04:19 +0800

    Create realistic_car_driving.lua

---
+++ Games/realistic_car_driving.lua
@@ -0,0 +1,38 @@
+-- Credits: github@lilmond
+-- How to use: Get into your car first before executing this script
+
+local player = game:GetService("Players").LocalPlayer
+local virtualuser = game:GetService("VirtualUser")
+
+local function auto_farm()
+    local character = player.Character
+    if not character then return end
+
+    local hrp = character:FindFirstChild("HumanoidRootPart")
+    if not hrp then return end
+
+    local car_weight = hrp:GetRootPart()
+    if car_weight == hrp then return end
+    
+    local car = car_weight.Parent.Parent
+
+    for i, wheel in pairs(car.Wheels:GetChildren()) do
+        wheel:Destroy()
+    end
+
+    while true do
+        if hrp:GetRootPart() == hrp then break end
+        hrp.AssemblyLinearVelocity = Vector3.new(0, 1000, 0)
+        hrp.CFrame = CFrame.new(0, 9e4, 0)
+        for i = 1, 100 do
+            if hrp:GetRootPart() == hrp then break end
+            hrp.AssemblyLinearVelocity = Vector3.new(0, 1000, 0)
+            wait(0.1)
+        end
+
+        virtualuser:CaptureController()
+        virtualuser:ClickButton2(Vector2.new())
+    end
+end
+
+auto_farm()
